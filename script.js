2ebdf5bff972c1badb522fad86531e40b59456d2
christianmesinas <48384755+christianmesinas@users.noreply.github.com>
2019-09-24 11:30:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+$(function () {
+  $(document).scroll(function(){
+    var $nav = $("#mainNavbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop()> $nav.height());
+  });
+});
+$('.navbar-nav>li>a').on('click', function(){
+$('.navbar-collapse').collapse('hide');
+});
+
+if ($('#back-to-top').length) {
+var scrollTrigger = 100, // px
+    backToTop = function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > scrollTrigger) {
+            $('#back-to-top').addClass('show');
+        } else {
+            $('#back-to-top').removeClass('show');
+        }
+    };
+backToTop();
+$(window).on('scroll', function () {
+    backToTop();
+});
+$('#back-to-top').on('click', function (e) {
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: 0
+    }, 700);
+});
+}
